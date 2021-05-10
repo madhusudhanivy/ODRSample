296e717205d3ae3550dd916544c255455e7bf01a
madhusudhanivy <madhusudhan.putta@ivycomptech.com>
2021-05-10 16:35:48 +0530

    NativeBridge Added

--- Source/Model.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Model.swift
-//  ODRSample
-//
-//  Created by Madhusudhan Reddy Putta on 07/05/21.
-//
-
-import Foundation
--- Source/Model1.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Model1.swift
-//  ODRSample
-//
-//  Created by Madhusudhan Reddy Putta on 10/05/21.
-//
-
-import Foundation
